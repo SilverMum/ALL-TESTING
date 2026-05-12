f5ef6ea695d1429d22ab3466f2b4d5970843b275
SilverMum <silvers.vibrant.ventures@outlook.com>
2026-05-11 21:55:29 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function openOverlay(id) {
-    document.getElementById(id).style.display = "flex";
-    document.body.classList.add("no-scroll");
-}
-
-function closeOverlay(id) {
-    document.getElementById(id).style.display = "none";
-    document.body.classList.remove("no-scroll");
-}
