d423bdb66cb99fc568d54a5f5bc7143af7431251
SilverMum <silvers.vibrant.ventures@outlook.com>
2026-05-11 21:45:39 -0400

    Create  activity.js

---
+++ scripts/activity.js
@@ -0,0 +1,13 @@
+<script>
+document.querySelectorAll('.mini-card').forEach(card => {
+    card.addEventListener('click', () => {
+        const target = card.getAttribute('data-target');
+
+        document.querySelectorAll('.activity-info').forEach(info => {
+            info.classList.remove('active');
+        });
+
+        document.getElementById(target).classList.add('active');
+    });
+});
+</script>
